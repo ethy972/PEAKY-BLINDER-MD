0c7226a61078f9e0512ac25a98560cea0a967b9b
ethy972 <kirwaben735@gmail.com>
2025-11-08 23:29:13 +0300

    Update SESSION_ID in settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~cMQ2iJAS#gU9FuF6MeD2WXpwJF5gon4ODhjC0O5OI4b9fDnx_yFo",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~NQoG1I5T#qAyb3zhxpiptlqafXKQD0rwaVXeYTLSF5rr8nJF5fl0",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
