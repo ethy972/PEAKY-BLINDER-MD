3e988909074e15786ed8199e6b6e24a880c46c3d
ethy972 <kirwaben735@gmail.com>
2025-11-10 12:47:19 +0300

    Update OWNER_NUMBER in settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: "blinder~QZ5GjDRY#UUv9VLWOb6msaANzjl_4TQYVhil0xfNUZ0MHtvI15i8",  // add your Session Id make sure it starts with blinder~
-  OWNER_NUMBER: "254721350988", // PUT OWNER NUMBER HERE
+  OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
